3a6e14c2ce570f8191d639beaaec66739431fd5a
FarelEllely <Edy.ellely@gmail.com>
2017-07-09 03:21:31 +0530

    Association Model

--- apps/association/models.py
+++ apps/association/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Association(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    year_of_creation = models.DateField(auto_now=False, auto_now_add=False)
+    address = models.CharField(max_length=100)
+    phone = models.IntergerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
