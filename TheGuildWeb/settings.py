88917a891a7e7f3fbea5c84d370749d160b809d5
axelkolo <gelyn.mandossi@gmail.com>
2017-07-09 05:24:01 +0530

    created Topic & Post models

--- TheGuildWeb/settings.py
+++ TheGuildWeb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.Forum',
     'apps.association',
     'django.contrib.admin',
     'django.contrib.auth',
--- apps/Forum/models.py
+++ apps/Forum/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length = 200)
+    status = models.BooleanField(default = False )
+    description = models.TextField(blank = True )
+    created_at = models.DateTimeField('auto_now_add = True')
+    updated_at = models.DateTimeField('auto_now = True')
+    reply = models.TextField(blank = True)
+
+
+class Post(models.Model):
+    name = models.CharField(max_length = 200)
+    description = models.TextField(blank = True)
+    image = models.ImageField(upload_to='Post')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
