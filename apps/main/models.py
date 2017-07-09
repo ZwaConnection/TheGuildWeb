11e2c6450152ef2477536dbca4c25370df8dd8df
ChanservainIbaka <chanservain.ibaka@gmail.com>
2017-07-09 07:53:30 +0530

    Announcement App

--- apps/main/models.py
+++ apps/main/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+#from apps.member.models import Profile
+
+from ..association.models import Association
+
+class Announcement(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    date = models.DateField()
+    picture = models.ImageField(upload_to='Images/Announcement/picture', blank=True)
+    #user = models.ForeignKey(User)
+    association = models.ForeignKey(Association)
