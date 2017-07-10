e9e8f37b827c6ac394bf7f43f8bb1fe6587dcd66
ChanservainIbaka <chanservain.ibaka@gmail.com>
2017-07-10 05:45:43 +0530

    updated the main

--- apps/Forum/views.py
+++ apps/Forum/views.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def forum(request):
+    return render(request, 'forum/forum.html')
+
+def topic_page(request):
+    return render(request, 'forum/topic_page.html')
+
+def posts(request):
+    return render(request, 'forum/posts.html')
+
+def specific_post_page(request):
+    return render(request, 'forum/specific_post_page.html')
+
+def post_creation_page(request):
+    return render(request, 'forum/post_creation_page.html')
--- apps/association/views.py
+++ apps/association/views.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def register(request):
+    return render(request, 'association/register.html')
+
+def ass_profile(request):
+    return render(request, 'association/profile.html')
+
+def account(request):
+    return render(request, 'association/account.html')
