e9a86ea5f48b85575fb31133c48f92deaf65c007
ChanservainIbaka <chanservain.ibaka@gmail.com>
2017-07-10 05:41:20 +0530

    updated views.py

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'app/index.html')
+def about(request):
+    return render(request, 'app/about.html')
+def contact(request):
+    return render(request, 'app/contact_us.html')
+def news(request):
+    return render(request, 'app/news.html')
+def articles(request):
+    return render(request, 'app/articles.html')
+def article_page(request):
+    return render(request, 'app/article_page.html')
+def feedback(request):
+    return render(request, 'app/feedback.html')
+def announcement_page(request):
+    return render(request, 'app/announcement_page.html')
+def policy(request):
+    return render(request, 'app/policy.html')
+def chat(request):
+    return render(request, 'app/chat.html')
+def terms_conditions(request):
+    return render(request, 'app/terms_conditions.html')
