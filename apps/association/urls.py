df1b410a9f855fe51c3f91cebe87df254c28df4d
FarelEllely <Edy.ellely@gmail.com>
2017-07-10 20:40:04 +0530

    Add association urls

---
+++ apps/association/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.register, name='register'),
+    url(r'^profile/$', views.ass_profile, name='ass_profile'),
+    url(r'^account/$', views.account, name='account'),
+]
