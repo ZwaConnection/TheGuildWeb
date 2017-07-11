d022243878fbed184b83b3ab6833d4c74f95054b
axelkolo <gelyn.mandossi@gmail.com>
2017-07-11 20:55:51 +0530

    Forum urls

--- TheGuildWeb/urls.py
+++ TheGuildWeb/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^association/', include('apps.association.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^forum/', include('apps.Forum.urls')),
 ]
--- apps/Forum/urls.py
+++ apps/Forum/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^forum/$', views.forum, name = 'forum'),
+    url(r'^$', views.forum, name = 'forum'),
     url(r'^topic_page/$', views.topic_page, name = 'topic_page'),
     url(r'^posts/$', views.posts, name = 'posts'),
     url(r'^specific_post_page/$', views.specific_post_page, name = 'specific_post_page'),
