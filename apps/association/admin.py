2103de309743e7179bfebaf2bbf09090899fb8f3
FarelEllely <Edy.ellely@gmail.com>
2017-07-16 03:11:38 +0530

    Fix association registration

--- apps/association/admin.py
+++ apps/association/admin.py
@@ -5,4 +5,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Association)
+# admin.site.register(Association)
+
+@admin.register(Association)
+class AssociationAdmin(admin.ModelAdmin):
+	list_display = ('identifier', 'name', 'created_at', 'updated_at',)
