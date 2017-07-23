0d7cf6e2af17ddf719e073ffd8b145e1d90fa775
godfather68 <edy.ellely@gmail.com>
2017-07-23 19:36:05 +0530

    add the .js back

--- apps/main/static/js/home.js
+++ apps/main/static/js/home.js
@@ -1,1 +1,29 @@
+$(document).ready(function(){
+  console.log('Loaded');
 
+  $('input#id_initials').change(function(){
+      $('input#association_id').val($(this).val());
+  });
+
+
+  console.log($('input#association_id').val());
+  // console.log($('input#identifier_id').val());
+  //
+  // $('#terms').click(function(){
+  //   // check if the checkbox is checked
+  //   if($(this).is(':checked')){
+  //     $('#registerBtn').removeAttr('disabled');
+  //   }else{
+  //     $('#registerBtn').attr('disabled', 'disabled');
+  //   }
+  // });
+  //
+  $(function(){
+    $('#id_year_of_creation').datepicker({ dateFormat: 'yy-mm-dd'});
+  });
+
+  $(function(){
+    $('#id_dob').datepicker({dateFormat: 'yy-mm-dd'})
+  });
+
+});
