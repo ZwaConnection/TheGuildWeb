48fb0fe6ecb5d6026e63b4f5285c76873430de4f
godfather68 <edy.ellely@gmail.com>
2017-08-06 00:58:29 +0530

    @anonymous decorator for authenticated users

--- apps/member/views.py
+++ apps/member/views.py
@@ -14,6 +14,7 @@ from .forms import *
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .decorators import anonymous_required
 
 # create a function to resolve email to username
 def get_user(email):
@@ -23,6 +24,7 @@ def get_user(email):
         return None
 
 # create a view that authenticate user with email
+@anonymous_required
 def login(request):
     context = {}
     # email = request.POST['email']
@@ -40,6 +42,7 @@ def login(request):
 def profile(request):
     return render(request, 'member/user_profile.html')
 
+@anonymous_required
 def user_register(request):
     countries = Country.objects.all()
     country_list = [ c.country for c in countries ]
